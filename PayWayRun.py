812301b3ca4e1679e53129019a3fe5842bbbb303
Martin Correa <martin.correa@redb.ee>
2022-07-13 22:20:51 -0300

    refact

--- PayWayRun.py
+++ PayWayRun.py
@@ -6,6 +6,10 @@ arguments = sys.argv
 root = os.environ.get("FRONT") + "/"
 command = " -e yarn " + arguments[1]
 
+def runMicroFront( paths, comnd ):
+    newTab = "tilix -a session-add-down -w "
+    os.system(newTab + paths + comnd)
+
 if __name__ == "__main__":
 
     for project in os.listdir(root):
@@ -13,10 +17,10 @@ if __name__ == "__main__":
         path = root + project
 
         if project == "ui-backoffice":
-            os.system("tilix -a session-add-down -w " + path + command)
-            os.system("tilix -a session-add-down -w " + path + "/layout" + command)
+            runMicroFront( path , command )
+            runMicroFront( path + "/layout" , command )
         elif project == "ui-payway-styleguide":
-            os.system("tilix -a session-add-down -w " + path )
-            os.system("tilix -a session-add-down -w " + path )
+            runMicroFront( path , "" )
+            runMicroFront( path , "" )
         else:    
-            os.system("tilix -a session-add-down -w " + path + command)
+            runMicroFront( path , command )
