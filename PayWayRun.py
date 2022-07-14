5c6b8d647a5be0f9258c1b8c27f48c050938718c
Martin Correa <martin.correa@redb.ee>
2022-07-14 15:41:24 -0300

    refactor logger

--- PayWayRun.py
+++ PayWayRun.py
@@ -6,31 +6,31 @@ arguments = sys.argv
 root = os.environ.get("FRONT") + "/"
 command = " -e yarn " + arguments[1]
 
-def runMicroFront( paths, comnd ):
+def runMicroFront( structure ):
     newTab = "tilix -a session-add-down -w "
+    paths = structure[0]
+    comnd = structure[1]
     if paths.__contains__("ui-payway-styleguide"):
-        os.system(newTab + paths + comnd)
-        os.system(newTab + paths + comnd)
+        os.system(newTab + paths)
+        os.system(newTab + paths)
     elif paths == root + "ui-backoffice":
         os.system(newTab + paths + comnd)
         os.system(newTab + paths + "/layout" + comnd)
     else:
         os.system(newTab + paths + comnd)
 
-def verifyProject(project):
-    if project == "ui-payway-styleguide":
-        print("sin comando")
-        return ""
-    else:
-        print("corre comando" + command[3:])
-        return command
+def buildPathAndCommand( project ):
+    print("_______________________________________")
+    print("·Abriendo " + project)
+    path = root + project
+    print("   -corre comando" + command[3:])
+    print("_______________________________________")
+    print("")
+    return path , command
 
-def path(project):
-    print("Abriendo " + project)
-    return root + project
 
 if __name__ == "__main__":
 
     proyectosList = os.listdir(root)
 
-    list( map(runMicroFront , map( path , proyectosList ) , map(verifyProject , proyectosList ) ))
+    list( map( runMicroFront , map( buildPathAndCommand , proyectosList ) ))
