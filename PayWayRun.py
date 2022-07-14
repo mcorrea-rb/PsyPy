92c5cb40f54ff5bd2d167f4a99cc52a999ec9f50
Martin Correa <martin.correa@redb.ee>
2022-07-14 16:10:17 -0300

    minor change

--- PayWayRun.py
+++ PayWayRun.py
@@ -1,36 +1,59 @@
 import os
 import sys
 
-arguments = sys.argv
+param = sys.argv[1]
 
 root = os.environ.get("FRONT") + "/"
-command = " -e yarn " + arguments[1]
+
+
+def run(query):
+    os.system(query)
 
 def runMicroFront( structure ):
     newTab = "tilix -a session-add-down -w "
     paths = structure[0]
     comnd = structure[1]
-    if paths.__contains__("ui-payway-styleguide"):
-        os.system(newTab + paths)
-        os.system(newTab + paths)
-    elif paths == root + "ui-backoffice":
-        os.system(newTab + paths + comnd)
-        os.system(newTab + paths + "/layout" + comnd)
-    else:
-        os.system(newTab + paths + comnd)
+    project = structure[2]
 
-def buildPathAndCommand( project ):
     print("_______________________________________")
     print("·Abriendo " + project)
+    if project=="ui-payway-styleguide":
+        print("   -sin comando")
+        print("_______________________________________")
+        print("")
+        run(newTab + paths)
+        print("_______________________________________")
+        print("·Abriendo " + project)
+        print("   -sin comando")
+        print("_______________________________________")
+        print("")
+        run(newTab + paths)
+    elif project == "ui-backoffice":
+        print("   -corre comando" + comnd[3:])
+        print("_______________________________________")
+        print("")
+        run(newTab + paths + comnd)
+        print("_______________________________________")
+        print("·Abriendo " + project + "/layout")
+        print("   -corre comando" + comnd[3:])
+        print("_______________________________________")
+        print("")
+        run(newTab + paths + "/layout" + comnd)
+    else:
+        print("   -corre comando" + comnd[3:])
+        print("_______________________________________")
+        print("")
+        run(newTab + paths + comnd)
+    
+
+def buildPathAndCommand( project ):
+    command = " -e yarn " + param
     path = root + project
-    print("   -corre comando" + command[3:])
-    print("_______________________________________")
-    print("")
-    return path , command
+    return path , command , project
 
 
 if __name__ == "__main__":
 
     proyectosList = os.listdir(root)
 
-    list( map( runMicroFront , map( buildPathAndCommand , proyectosList ) ))
+    list( map( runMicroFront , map( buildPathAndCommand , proyectosList ) ) )
