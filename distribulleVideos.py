d98d21adf6cb7084b74e482325919c55eaeeec8b
Martin Correa <martin.correa@redb.ee>
2022-07-17 20:10:05 -0300

    refactor distribuidor de videos

--- distribulleVideos.py
+++ distribulleVideos.py
@@ -7,37 +7,17 @@ grabacionesFolder = root+"PsyBrainy/Grabaciones"
 if __name__ == "__main__":
 
     def getMonthName(month):
-        año = month[:5]
         mes = ""
-        mes2 = int(month[6:])
+        mesNumero = int(month[6:])
 
-        if mes2 == 1:
-            mes = "Enero"
-        elif mes2 == 2:
-            mes = "Febrero"
-        elif mes2 == 3:
-            mes = "Marzo"
-        elif mes2 == 4:
-            mes = "Abril"
-        elif mes2 == 5:
-            mes = "Mayo"
-        elif mes2 == 6:
-            mes = "Junio"
-        elif mes2 == 7:
-            mes = "Julio"
-        elif mes2 == 8:
-            mes = "Agosto"
-        elif mes2 == 9:
-            mes = "Septiembre"
-        elif mes2 == 10:
-            mes = "Octubre"
-        elif mes2 == 11:
-            mes = "Noviembre"
-        elif mes2 == 12:
-            mes = "Diciembre"
-        else:
-            mes = "Error"
-        return año + "-" + str(mes2) + "-" + mes
+        mesesresp = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+        
+        for i in range(len(mesesresp)):
+            if mesNumero == i+1:
+                mes = mesesresp[i]
+                break
+
+        return month + "-" + mes
 
     os.makedirs(grabacionesFolder, exist_ok=True)
     for filename in os.listdir(root):
